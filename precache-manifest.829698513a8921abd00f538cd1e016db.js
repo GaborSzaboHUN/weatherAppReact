82215b3274ed65edda3b3c37e138f9075744cc3f
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-12-10 01:11:51 +0100

    Updates

---
+++ precache-manifest.829698513a8921abd00f538cd1e016db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af4950f5595484eda98f",
+    "url": "/weatherAppReact/static/css/main.6a0d09aa.chunk.css"
+  },
+  {
+    "revision": "af4950f5595484eda98f",
+    "url": "/weatherAppReact/static/js/main.af4950f5.chunk.js"
+  },
+  {
+    "revision": "a0e643d4d8d07acd9444",
+    "url": "/weatherAppReact/static/js/1.a0e643d4.chunk.js"
+  },
+  {
+    "revision": "ef336108e49557362eb5",
+    "url": "/weatherAppReact/static/js/runtime~main.ef336108.js"
+  },
+  {
+    "revision": "a21f7a137a899faf1cf584909c3bbd35",
+    "url": "/weatherAppReact/index.html"
+  }
+];
